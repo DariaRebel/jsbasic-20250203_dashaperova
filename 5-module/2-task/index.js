d2805516ec1d55efa4708406f62e42409d71df9e
Dasha Perova <dashulisaperovaegmail.com>
2025-03-05 13:23:13 +0300

    5 модуль все задачи

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let elem = document.getElementById('text');
+  document.querySelector('.toggle-text-button').onclick = function() {
+    if (elem.hidden == null) {
+      elem.hidden = true;
+    } else elem.hidden = !elem.hidden;
+  } 
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
   // ваш код...
+  let count = 0;
+  let position = 0;
+  let elem = document.querySelector(".carousel__inner");
+  let width = elem.offsetWidth;
+  let rightnow = document.querySelector('.carousel__arrow.carousel__arrow_right');
+  let leftnow =document.querySelector('.carousel__arrow.carousel__arrow_left');
+  if (count == 0) leftnow.style.display = 'none';
+
+  rightnow.onclick = function() {
+    if (count < 3) {
+      count += 1;
+      leftnow.style.display = '';
+      position -= width;
+      elem.style.transform = `translateX(${position}px)`;
+      if (count == 3) rightnow.style.display = 'none';
+    }
+  }
+
+    leftnow.onclick = function() {
+      if (count > 0 ) {
+        count -= 1;
+        rightnow.style.display = '';
+        position += width;
+        elem.style.transform = `translateX(${position}px)`;
+        if (count == 0) leftnow.style.display = 'none';
+      }
+    }
 }
